fb61370c22812e4ee268a6fd4bfeaeffd1c1f81c
hunterZh37 <71850140+hunterZh37@users.noreply.github.com>
2022-07-10 21:31:34 -0500

    3

---
+++ Final Project/Assets/Scripts/StationMotion.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationMotion : MonoBehaviour
+{
+    public float speed;
+    
+    void Update()
+    {
+        transform.Rotate(0f, speed, 0f);
+    }
+}
